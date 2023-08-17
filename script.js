9f75f282d9081b9e20ea9eb14144334645157ed5
I'M JonathanRafael <93375694+Jonathancyber@users.noreply.github.com>
2023-08-17 19:26:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth scroll animation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Contact form submission
+function sendMessage() {
+    alert('Message sent successfully!');
+}
